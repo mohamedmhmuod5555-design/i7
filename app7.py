69bb047661c6b078fa5525424c33ec337f622d80
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-24 16:03:29 -0700

    Update app7.py

--- app7.py
+++ app7.py
@@ -3,8 +3,7 @@ import streamlit as st
 num=0
 if num1 not in st.session_state:
  st.session_state.num1()==random.randint(1,20)
- st.session_state.num2()==random.randint(1,20)
- st.session_state.sign()==random.choice(['+','-','*','/'])
+ 
 num1=st.session_state.num1()
 num2=st.session_state.num2()
 sign=st.session_state.sign()
